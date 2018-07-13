7427aec1153262614f569af0aa7dadfb05101174
coianis leonardo <leonardo.coianis@gmail.com>
2018-07-13 12:57:21 +0200

    initial commit

---
+++ AppMobile/Controllers/ManualController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Devices;
+
+namespace AppMobile.Controllers
+{
+    public class ManualController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task Feed()
+        {
+            var id = "";
+            var serviceClient = ServiceClient.CreateFromConnectionString("asdfja");
+            var bytes = Encoding.UTF8.GetBytes("Feed");
+            var message = new Message(bytes);
+
+            await serviceClient.SendAsync(id, message);
+        }
+
+        public async Task Foto()
+        {
+            var id = "";
+            var serviceClient = ServiceClient.CreateFromConnectionString("asdfja");
+            var bytes = Encoding.UTF8.GetBytes("Foto");
+            var message = new Message(bytes);
+
+            await serviceClient.SendAsync(id, message);
+        }
+
+        public async Task<JsonResult> GetUrlFoto()
+        {
+            var id = "";
+            var rm = RegistryManager.CreateFromConnectionString("");
+            var twin = await rm.GetTwinAsync(id);
+
+            var valore = twin.Properties.Desired["imageUrl"];
+
+            return Json(new { value = valore });
+        }
+    }
+}
---
+++ ManualConnectedCiotola/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Shared;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+
+namespace ManualConnectedCiotola
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            int _dosi;
+
+            var builder = new ConfigurationBuilder()
+              .SetBasePath(Directory.GetCurrentDirectory())
+              .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            var config = builder.Build();
+            var deviceId = config["deviceId"];
+            var authenticationMethod = new DeviceAuthenticationWithRegistrySymmetricKey(deviceId, config["key"]);
+            var transportType = TransportType.Mqtt_WebSocket_Only;
+            
+            var client = DeviceClient.Create(deviceId, authenticationMethod, transportType);
+
+
+            while (true)
+            {
+                Message receivedMessage = await client.ReceiveAsync();
+                if (receivedMessage == null)
+                {
+                    continue;
+                } else
+                {
+                    var message = Encoding.UTF8.GetString(receivedMessage.GetBytes());
+
+                    if(message == "Feed")
+                    {
+                        var twin = await client.GetTwinAsync();
+                        if (twin.Properties.Reported.Contains("dosi"))
+                        {
+                            _dosi = (int)twin.Properties.Reported["dosi"];
+                            if (_dosi > 0)
+                            {
+                                _dosi--;
+                            }
+                        } else
+                        {
+                            _dosi = 10;
+                        }
+
+                        var nTwin = new TwinCollection();
+                        nTwin["dosi"] = _dosi;
+                        await client.UpdateReportedPropertiesAsync(nTwin);
+
+                    }
+                    else if (message == "Foto")
+                    {
+                        var connectionString = config["StorageConnectionString"];
+                        var storageAccount = CloudStorageAccount.Parse(connectionString);
+                        var blobClient = storageAccount.CreateCloudBlobClient();
+                        var imageContainer = blobClient.GetContainerReference("images");
+                        var blob = imageContainer.GetBlockBlobReference("image.jpg");
+                        await blob.UploadFromFileAsync("image.jpg");
+                        var blobUrl = blob.StorageUri.PrimaryUri.ToString();
+
+                        var nTwin = new TwinCollection();
+                        nTwin["imageUrl"] = blobUrl;
+                        await client.UpdateReportedPropertiesAsync(nTwin);
+                    }
+                }
+
+            }
+        }
+    }
+}
